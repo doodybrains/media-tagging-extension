70651291d4ce034bdfcbecd6722fd6e9e573e17e
doodybrains <doodybrainz@gmail.com>
2018-08-30 15:59:30 -0400

    Update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-const contentful = require('contentful')
-const mgmt = require('contentful-management')
+const axios = require('axios');
 
 window.contentfulExtension.init(initExtension);
 
-const cda = contentful.createClient({space: '', accessToken: ''})
+var config = {
+  headers: {'Accept': 'application/json', 'Content-Type': 'application/json',}
+};
 
 function initExtension(extension) {
   extension.window.updateHeight();
   extension.window.startAutoResizer();
+
   if (extension.entry.fields.imageTags) {
     getAssets(extension);
   }
@@ -16,64 +18,30 @@ function initExtension(extension) {
 function getAssets(extension) {
   const sys = extension.entry.getSys();
   if (sys) {
-    // hit heroku app with sys.id
-    // get response and send through to getAssets
-    cda.getEntry(sys.id).then(response => {
-      updateAssets(response)
-    }).catch(console.error)
+    axios.post('http://localhost:3000/', { sysId: 'jaw9YaHoukG60i6mce242' }, config)
+    .then(function(response){
+       updateAssets(response)
+     });
   }
 }
 
 function updateAssets(assets) {
   const mainContainer = document.getElementById('assets');
 
-  assets.fields.images.forEach(function (fi) {
+  assets.data.fields.images.forEach(function (fi) {
     const id = fi.sys.id;
-    const tagId = assets.sys.id
+    const tagId = assets.data.sys.id
 
     mainContainer.insertAdjacentHTML('beforeend', `<div class="thumb" id="${id}-${tagId}"><img src=${fi.fields.file.url} /></thumb>` );
-    buildEntries(`${fi.sys.id}`, `${assets.fields.imageTags}`, `${assets.sys.id}`, `${fi.sys.revision}`)
+    buildEntries(`${fi.sys.id}`, `${assets.data.fields.imageTags}`, `${assets.data.sys.id}`, `${fi.sys.revision}`)
   })
 }
 
-function buildEntries(asset_id, tags, tagId, version) {
-  const client = mgmt.createClient({accessToken: '', headers: {'X-Contentful-Version': version}})
-
-  client.getSpace('l93sefy83g83')
-  .then((space) => space.getEnvironment('master'))
-  .then((environment) => environment.getAsset(asset_id))
-  .then((asset) => {
-    if (!asset.fields.description) {
-      asset.fields.description = {'en-US': ''};
-    }
-
-    const des = asset.fields.description['en-US'];
-    let description = `${des}`;
-    const tagWords = ` ${tags}`;
-    let newDescription = '';
-
-    if (des.includes(tagWords)) {
-      return asset;
-    } else {
-      description = `${des}`;
-      newDescription = description.concat(tagWords);
-      asset.fields.description['en-US'] = newDescription
-      return asset.update()
-    }
-  })
-  .then((asset) => {
-    if (asset.isUpdated()) {
-      return asset.publish()
-    } else {
-      return asset;
-    }
-  })
-  .then((asset) => {
-    const assetThumb = document.getElementById(`${asset_id}-${tagId}`);
-    if (asset.isPublished()) {
-      assetThumb.classList.add('published');
-      return asset;
-    }
-  })
-  .catch(console.error)
+function buildEntries(asset_id, tags, tag_id, version) {
+  axios.post('http://localhost:3000/build', {assetId: asset_id, allTags: tags, versionNo: version}, config)
+  .then(function(response) {
+     console.log(response);
+     const assetThumb = document.getElementById(`${asset_id}-${tag_id}`);
+     assetThumb.classList.add('published');
+   });
 }
